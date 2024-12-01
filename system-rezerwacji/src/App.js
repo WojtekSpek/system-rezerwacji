a407855099e8d766f5e47f9d7d3d7a15edee39aa
Wojtek Adamiak <wojtek@studiospektrum.pl>
2024-12-01 18:32:44 +0100

    Initial commit

---
+++ system-rezerwacji/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Witaj w React!</h1>
+      <p>To jest twoja pierwsza aplikacja React</p>
+    </div>
+  );
+}
+
+export default App;
