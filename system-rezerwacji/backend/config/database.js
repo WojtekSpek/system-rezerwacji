43dab6b3731dfe60d35a2a790188dada34549917
wojtek <wojtek@studiospektrum.pl>
2024-12-30 13:09:16 +0100

    sdsd

--- system-rezerwacji/backend/config/database.js
+++ system-rezerwacji/backend/config/database.js
@@ -1,17 +1,14 @@
 require('dotenv').config(); // Ładowanie zmiennych środowiskowych
 const mysql = require("mysql2");
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
+
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.PORT,
+  //port: process.env.PORT,
 }); 
 
 /* const db = mysql.createPool({
--- system-rezerwacji/backend/server.js
+++ system-rezerwacji/backend/server.js
@@ -43,6 +43,11 @@ app.use(session({
   },
 }));
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 // Rejestracja tras
 app.use("/projects", projectRoutes);
 app.use("/participants", participantRoutes);
