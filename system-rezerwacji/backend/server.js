2afbea72d2934ef1d338a00be9a1ac0d0564d741
wojtek <wojtek@studiospektrum.pl>
2024-12-30 13:03:21 +0100

    dfdf

--- system-rezerwacji/backend/server.js
+++ system-rezerwacji/backend/server.js
@@ -55,6 +55,7 @@ app.use("/group", groupRoutes);
 app.use("/skills", skillsRoutes);
 
 // Uruchomienie serwera
-app.listen(process.env.PORT, () => {
-  console.log(`Serwer działa na porcie ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
