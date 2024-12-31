065d9d9e6ad0af4cc387c5011cb95fbabdb14af1
wojtek <wojtek@studiospektrum.pl>
2024-12-31 15:09:53 +0100

    dziala ale bez sesji

--- system-rezerwacji/backend/server.js
+++ system-rezerwacji/backend/server.js
@@ -30,13 +30,13 @@ app.use(cors({
 
 const path = require("path");
 
-/* // Serve static files from the React app
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Catch-all handler for any requests that don’t match API routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-}); */
+});
 
 
 // Middleware
