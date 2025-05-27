4d69067bf8310841149528bd445e2eb3a4c193fb
luke-mag <lukers3maill@gmail.com>
2025-05-27 15:37:27 +0200

    DONE: Odświrerzenie i build

--- system-rezerwacji/backend/server.js
+++ system-rezerwacji/backend/server.js
@@ -19,8 +19,7 @@ const skillsRoutes = require("./routes/skills");
 ? process.env.REACT_APP_API_BASE_URL + ':' + process.env.CLIENT_PORT
   : process.env.REACT_APP_HOST_LAN_URL + ':' + process.env.CLIENT_PORT; */
 
-const API_BASE_URL = ['https://plan.myappspot.eu'];
-
+const API_BASE_URL = ['https://plan.myappspot.eu:3000'];
 const app = express();
 const PORT = process.env.PORT || 5000; // Lokalnie 5000, na Render użyje zmiennej środowiskowej PORT
 
