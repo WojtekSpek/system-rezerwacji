e3347575f83a5737190ede4b6201355e4be00d3f
luke-mag <lukers3maill@gmail.com>
2025-05-28 11:12:25 +0200

    DONE: serwer session httpOnly = true

--- system-rezerwacji/backend/server.js
+++ system-rezerwacji/backend/server.js
@@ -50,7 +50,7 @@ app.use(session({
   saveUninitialized: true,
   cookie: {
     secure: true,//eśli używasz HTTPS
-    httpOnly: false,
+    httpOnly: true,
     sameSite: "none",
     domain: ".myappspot.eu",
   },
